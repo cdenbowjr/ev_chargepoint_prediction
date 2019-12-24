164bb5f35753b13d6cc9027f640e2af4dfb59dd8
cdenbowjr <54248420+cdenbowjr@users.noreply.github.com>
2019-12-24 08:06:35 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project is about predicting EV chargepoints in England',
+    author='Charles Denbow',
+    license='',
+)
